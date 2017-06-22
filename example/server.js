8185dcfde80708258f6ed42e5a46d1e39931d8a3
Noam Shemesh <noams@jfrog.com>
2017-06-22 13:57:27 +0300

    Fix some issues. Now can run with latest express and everyauth

--- example/server.js
+++ example/server.js
@@ -1,125 +1,129 @@
-var SsoJuggler = require('../lib/ssoJuggler');
+const SsoJuggler = require('../lib/ssoJuggler');
 
-var authPath= '/auth',
-    deauthPath= '/deauth',
-    successPath= '/success',
-    validatePath= '/val';
-    
-var ssoJuggler = new SsoJuggler({
-        authenticationPath: '/login',
-        //authenticationPath: '/auth/openid?openid_identifier=https://www.google.com/accounts/o8/id',
-        cookieExpirationTime: 20,
-        authPath: authPath,
-        deauthPath: deauthPath,
-        successPath: successPath,
-        validatePath: validatePath
-    });
+const authPath= '/auth'
+const deauthPath= '/deauth'
+const successPath= '/success'
+const validatePath= '/val'
+const everyauth = require('everyauth')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const app = express()
 
-var everyauth = require('everyauth');
+const ssoJuggler = new SsoJuggler({
+  authenticationPath: '/login',
+  cookieExpirationTime: 20,
+  authPath: authPath,
+  deauthPath: deauthPath,
+  successPath: successPath,
+  validatePath: validatePath
+});
 
-//everyauth.debug = true;
-everyauth
-  .openid
-    .myHostname('http://localhost:3001')
-    .findOrCreateUser( function (session, userMetadata) {
+initEveryauth()
+initExpress()
 
-      // Don't forget to save the userIdentifier!
-      ssoJuggler.saveUserIdentifier(session, userMetadata.email);
-      ssoJuggler.saveAuthSource(session, 'openId');
-      
-      if (userMetadata.claimedIdentifier.indexOf('https://www.google.com/accounts/o8/id') === 0) {
-          ssoJuggler.saveAuthSource(session, 'google');
-      }
-      
-      return userMetadata;
-    })
+function initEveryauth() {
+  const authenticate = (login, password, params) => {
+    console.log(login);
+    console.log(password);
+    console.log(params.req.params.remember !== undefined);
+
+    const errors = [];
+    if (!login) errors.push('Missing login');
+    if (!password) errors.push('Missing password');
+    if (errors.length) return errors;
+    const user = { login: 'user', password: 'password'};
+    if (!user) return ['Login failed'];
+    if (user.password !== password) return ['Login failed'];
+
+    ssoJuggler.saveUserIdentifier(params.req.session, user.login);
+    ssoJuggler.saveAuthSource(params.req.session, 'password');
+    ssoJuggler.saveRemember(params.req.session, params.req.params.remember !== undefined);
+    //req.session.userIdentifier = user.login;
+
+    return user;
+  }
+
+  const findOrCreateUser = (session, userMetadata) => {
+
+    // Don't forget to save the userIdentifier!
+    ssoJuggler.saveUserIdentifier(session, userMetadata.email);
+    ssoJuggler.saveAuthSource(session, 'openId');
+
+    if (userMetadata.claimedIdentifier.indexOf('https://www.google.com/accounts/o8/id') === 0) {
+        ssoJuggler.saveAuthSource(session, 'google');
+    }
+
+    return userMetadata;
+  }
+
+  everyauth.openid.myHostname('http://localhost:3001')
+    .findOrCreateUser(findOrCreateUser)
     .redirectPath(successPath);
-everyauth.password
-  .loginWith('login')
-  .getLoginPath('/login') // Uri path to the login page
-  .postLoginPath('/login') // Uri path that your login form POSTs to
-  .loginView("login.jade")
-  .extractExtraRegistrationParams( function (req) {
-      return req;
-  })
-  .authenticate( function (login, password, req) {
-      
-      console.log(login);
-      console.log(password);
-      console.log(req.param('remember') !== undefined);
-      
-      var errors = [];
-      if (!login) errors.push('Missing login');
-      if (!password) errors.push('Missing password');
-      if (errors.length) return errors;
-      var user = { login: 'user', password: 'password'};
-      if (!user) return ['Login failed'];
-      if (user.password !== password) return ['Login failed'];
-            
-      ssoJuggler.saveUserIdentifier(req.session, user.login);
-      ssoJuggler.saveAuthSource(req.session, 'password');
-      ssoJuggler.saveRemember(req.session, req.param('remember') !== undefined);
-      //req.session.userIdentifier = user.login;
-      
-      return user;
-  })
-  .loginSuccessRedirect(successPath) // Where to redirect to after a login
 
-  .getRegisterPath('/register') // Uri path to the registration page
-  .postRegisterPath('/register') // The Uri path that your registration form POSTs to
-  .registerView('a string of html; OR the name of the jade/etc-view-engine view')
-  .validateRegistration( function (newUserAttributes) {
-  })
-  .registerUser( function (newUserAttributes) {
-  })
-  .registerSuccessRedirect(successPath); // Where to redirect to after a successful registration
-  
-//everyauth.everymodule.logoutRedirectPath(deauthPath);
-  
-var express = require('express');
-//var RedisStore = require('connect-redis')(express);
-var app = express.createServer(
-    express.bodyParser()
-  , express.static(__dirname + '/public')
-  , express.cookieParser()
-  , express.session({ secret: 'htuayreve'/*, store: new RedisStore */})
-  , everyauth.middleware()
-);
+  everyauth.password
+    .loginWith('login')
+    .loginFormFieldName('login')
+    .passwordFormFieldName('password')
+    .getLoginPath('/login') // Uri path to the login page
+    .postLoginPath('/login') // Uri path that your login form POSTs to
+    .loginView('../example/views/login.jade')
+    .extractExtraRegistrationParams((req) => req)
+    .authenticate(authenticate)
+    .loginSuccessRedirect(successPath) // Where to redirect to after a login
+    .getRegisterPath('/register') // Uri path to the registration page
+    .postRegisterPath('/register') // The Uri path that your registration form POSTs to
+    .registerView('a string of html; OR the name of the pug/etc-view-engine view')
+    .validateRegistration(newUserAttributes => {})
+    .registerUser(newUserAttributes => {})
+    .registerSuccessRedirect(successPath); // Where to redirect to after a successful registration
+}
 
-ssoJuggler.addRoutes(app);
+function initExpress() {
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(express.static(__dirname + '/public'))
+  app.use(cookieParser())
+  app.use(session({ secret: 'htuayreve'/*, store: new RedisStore */ }))
+  app.use(everyauth.middleware())
+  app.set('view engine', 'pug')
 
-var consumerToken = 'testToken';
+  ssoJuggler.addRoutes(app);
 
-app.get('/', function(req, res){
+  const consumerToken = 'testToken';
+
+  app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Login <a href="'+authPath+'?consumerToken='+consumerToken+'&callbackUrl=http://localhost:3001/validate/">'+authPath+'?consumerToken='+consumerToken+'&callbackUrl=http://localhost:3001/validate</a>');
     res.write('</br>');
     res.write('</br>');
     res.write('Logout <a href="'+deauthPath+'?callbackUrl=http://www.google.ch">'+deauthPath+'?callbackUrl=http://www.google.ch</a>');
     res.end();
-});
+  });
 
-app.get('/validate', function(req, res){
-    var userToken = req.param('userToken');
+  app.get('/validate', function(req, res){
+    const userToken = req.param('userToken');
     res.redirect('/val?consumerToken=' + consumerToken + '&userToken=' + userToken + '&callbackUrl=http://localhost:3001/result');
-});
+  });
 
-app.get('/result', function(req, res){
-    var userIdentifier = req.param('userIdentifier');
-    var backConsumerToken = req.param('consumerToken');
+  app.get('/result', function(req, res){
+    const userIdentifier = req.param('userIdentifier');
+    const backConsumerToken = req.param('consumerToken');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     if (backConsumerToken == consumerToken) {
-        if (userIdentifier) {
-            res.write('This is the user: '+userIdentifier);
-        } else {
-            res.write('User not valid');
-        }
+      if (userIdentifier) {
+        res.write('This is the user: '+userIdentifier);
+      } else {
+        res.write('User not valid');
+      }
     } else {
         res.write('Wrong sender');
     }
     res.end();
-});
+  });
 
-everyauth.helpExpress(app);
+  everyauth.helpExpress(app);
 
-app.listen(3001);
+  app.listen(3001);
+}
